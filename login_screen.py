ebdc03e030df4dfa921237ba5cb826a4c9dfefc0
Nikolas Zavliaris <nikolaszav2003@gmail.com>
2025-05-27 13:40:50 +0300

    Login screen changed

--- login_screen.py
+++ login_screen.py
@@ -9,7 +9,7 @@ def launch_dashboard():
 
 root = tk.Tk()
 root.title("Σύνδεση")
-root.geometry("600x400")
+root.geometry("1000x700")
 
 login = LoginScreen(root, on_submit_callback=launch_dashboard)
 login.pack(fill="both", expand=True)
